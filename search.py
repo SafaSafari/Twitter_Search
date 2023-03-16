a077bab9f6f63acfd654c00a97260a6271194900
Safa Safari <safasafari.ss@gmail.com>
2023-03-16 23:58:01 +0330

    fix next page "replaceEntry"

--- search.py
+++ search.py
@@ -1,5 +1,9 @@
-import requests, re, json
+import requests
+import re
+import json
+import itertools
 from urllib.parse import urlencode
+
 
 class twitter:
     def __init__(self):
@@ -12,17 +16,54 @@ class twitter:
             if 'serviceworker' in js:
                 break
         jjs = requests.get(js).text
-        temp_api = re.search(r'authorization:"Bearer ".concat\("(.*)"\),"content-type"', jjs).group(1)
+        temp_api = re.search(
+            r'authorization:"Bearer ".concat\("(.*)"\),Accept', jjs).group(1)
         return temp_api
 
     def activate(self):
         return requests.post('https://api.twitter.com/1.1/guest/activate.json', headers={'Authorization': 'Bearer {}'.format(self.token)}).json()['guest_token']
 
     def search(self, query, page=''):
-        params = {"q": query, "include_quote_count": True, "tweet_search_mode": "live"}
+        params = {"include_profile_interstitial_type": 1,
+                  "include_blocking": 1,
+                  "include_blocked_by": 1,
+                  "include_followed_by": 1,
+                  "include_want_retweets": 1,
+                  "include_mute_edge": 1,
+                  "include_can_dm": 1,
+                  "include_can_media_tag": 1,
+                  "include_ext_has_nft_avatar": 1,
+                  "include_ext_is_blue_verified": 1,
+                  "include_ext_verified_type": 1,
+                  "skip_status": 1,
+                  "cards_platform": "Web-12",
+                  "include_cards": 1,
+                  "include_ext_alt_text": True,
+                  "include_ext_limited_action_results": False,
+                  "include_quote_count": True,
+                  "include_reply_count": 1,
+                  "tweet_mode": "extended",
+                  "include_ext_views": True,
+                  "include_entities": True,
+                  "include_user_entities": True,
+                  "include_ext_media_color": True,
+                  "include_ext_media_availability": True,
+                  "include_ext_sensitive_media_warning": True,
+                  "include_ext_trusted_friends_metadata": True,
+                  "send_error_codes": True,
+                  "simple_quoted_tweet": True,
+                  "q": query,
+                  "tweet_search_mode": "live",
+                  "query_source": "typed_query",
+                  "count": 20,
+                  "requestContext": "launch",
+                  "pc": 1,
+                  "spelling_corrections": 1,
+                  "include_ext_edit_control": True, }
         if page:
             params['cursor'] = page
         return requests.get('https://twitter.com/i/api/2/search/adaptive.json?{}'.format(urlencode(params)), headers={'Authorization': 'Bearer {}'.format(self.token), "X-Guest-Token": self.guest_id}).json()
+
 
 class search(twitter):
     def __init__(self, query, page=''):
@@ -35,16 +76,13 @@ class search(twitter):
         return self.last
 
     def get_next_page(self):
-        for i in self.last['timeline']['instructions'][0]['addEntries']['entries'][::-1]:
+        for i in itertools.chain(*[entries['addEntries']['entries'] if entries.get('addEntries') else [entries.get('replaceEntry').get('entry')] for entries in self.last['timeline']['instructions']]):
             if i['entryId'] == 'sq-cursor-bottom':
                 self.page = i['content']['operation']['cursor']['value']
                 return self.get_page()
 
     def get_prev_page(self):
-        for i in self.last['timeline']['instructions'][0]['addEntries']['entries']:
+        for i in itertools.chain(*[entries['addEntries']['entries'] if entries.get('addEntries') else [entries.get('replaceEntry').get('entry')] for entries in self.last['timeline']['instructions']]):
             if i['entryId'] == 'sq-cursor-top':
                 self.page = i['content']['operation']['cursor']['value']
                 return self.get_page()
-
-tw = search("from:@safasafari3")
-print(json.dumps(tw.get_page(), indent=4))
